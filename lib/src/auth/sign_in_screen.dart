c13eeb4f4c531072290b1484e05add5da997ffb1
Marcelo Corrêa <marcelocorrea229@gmail.com>
2022-07-13 16:00:30 -0300

    Flutter UI: Tela de Cadastro

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,8 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
+import 'package:greengrocer/src/base/base_screen.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -100,7 +102,13 @@ class SignInScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(18),
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          // Navigator.of(context).pushReplacement(
+                          //   MaterialPageRoute(builder: (_) {
+                          //     return const BaseScreen();
+                          //   }),
+                          // );
+                        },
                         child: const Text(
                           'Entrar',
                           style: TextStyle(
@@ -157,7 +165,13 @@ class SignInScreen extends StatelessWidget {
                             color: Colors.green,
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (_) {
+                              return const SignUpScreen();
+                            }),
+                          );
+                        },
                         child: const Text(
                           'Criar conta',
                           style: TextStyle(
